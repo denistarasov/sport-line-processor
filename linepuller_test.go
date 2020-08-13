e9b3a593e2e92166dd964e85b9c42011b4904471
Denis Tarasov <denistarasov@users.noreply.github.com>
2020-08-13 20:27:07 +0300

    Add test for linepuller

--- linepuller_test.go
+++ linepuller_test.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"github.com/stretchr/testify/require"
+	"sync"
 	"testing"
 )
 
-// todo
-func TestLinePuller_Simple(t *testing.T) {
+func TestLinePuller_IsReady(t *testing.T) {
+	s := newMapStorage()
+	lp := &linePuller{
+		Mutex:              sync.Mutex{},
+		linesProviderAddr:  "",
+		sportNames:         []string{"soccer", "football"},
+		storage:            s,
+		isLineProviderDown: false,
+		wg:                 nil,
+	}
+
+	require.Equal(t, notReady, lp.isReady())
+
+	s.Upload("soccer", 0)
+	require.Equal(t, notReady, lp.isReady())
+
+	s.Upload("soccer", 0)
+	require.Equal(t, notReady, lp.isReady())
+
+	s.Upload("football", 0)
+	require.Equal(t, ready, lp.isReady())
 }
